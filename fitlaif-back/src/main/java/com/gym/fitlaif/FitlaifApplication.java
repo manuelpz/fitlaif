6e9367ebaa40b27caa331cd5f9bab54ce88b2110
Manuel Lopez Fernandez <manuel.lopez.fernandez97@gmail.com>
2023-04-22 12:59:52 +0200

    Añadido back-end

---
+++ fitlaif-back/src/main/java/com/gym/fitlaif/FitlaifApplication.java
@@ -0,0 +1,13 @@
+package com.gym.fitlaif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitlaifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitlaifApplication.class, args);
+	}
+
+}
