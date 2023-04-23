508587c25abc7a4ef3e500bf40da6fc3ada24fce
Manuel Lopez Fernandez <manuel.lopez.fernandez97@gmail.com>
2023-04-24 00:41:52 +0200

    Refactorizacion del codigo para que aparezca mensaje de cargando mientras busca los datos

--- fitlaif-front/src/pages/entrenamientos/[id].tsx
+++ fitlaif-front/src/pages/entrenamientos/[id].tsx
@@ -4,26 +4,33 @@ import { useRouter } from 'next/router'
 export default function EntrenamientoFuncion() {
   const router = useRouter()
   const { id } = router.query
-  const [isMounted, setIsMounted] = useState(false)
-  const [data, setData] = useState<Entrenamiento | ''>('')
-
-  const fetchData = async () => {
-    const response = await fetch(`http://localhost:8080/entrenamientos/${id}`)
-    const data = await response.json()
-    setData(data)
-  }
+  const [data, setData] = useState<Entrenamiento | undefined>()
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-    fetchData()
-    setIsMounted(true)
+    const fetchData = async () => {
+      const response = await fetch(`http://localhost:8080/entrenamientos/${id}`)
+      const data = await response.json()
+      setData(data)
+      setIsLoading(false)
+    }
+
+    if (id) {
+      fetchData()
+    }
   }, [id])
 
-  if (!isMounted) {
-    return null
+  if (isLoading) {
+    return <div>Loading...</div>
   }
+
+  if (!data) {
+    return <div>No se encontró el entrenamiento.</div>
+  }
+
   return (
     <div>
-      <h1>El entrenamiento es {data ? data.musculo.toLowerCase() : ''}</h1>
+      <h1>El entrenamiento es {data.musculo.toLowerCase()}</h1>
     </div>
   )
 }
