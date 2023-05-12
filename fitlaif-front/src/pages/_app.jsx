3bd1d7cf0175a7afc76c848b3096f7b3c68e9242
Manuel Lopez Fernandez <manuel.lopez.fernandez97@gmail.com>
2023-05-12 18:08:07 +0200

    Creacion de la navbar e introduccion de iconos

--- fitlaif-front/src/pages/_app.jsx
+++ fitlaif-front/src/pages/_app.jsx
@@ -3,9 +3,11 @@ import '@/styles/modal.css'
 import Head from 'next/head'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Navbar from '../components/Navbar';
 export default function App({ Component, pageProps }) {
   return (
     <div>
+      <Navbar></Navbar>
       <Head>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css"></link>
       </Head>
